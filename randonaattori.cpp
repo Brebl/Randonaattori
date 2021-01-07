36528a52c642322356d5faee2ce02ad32e3f0988
Antti Helminen <antti@gsb.fi>
2021-01-07 17:48:29 +0200

    Update randonaattori.cpp

--- randonaattori.cpp
+++ randonaattori.cpp
@@ -14,32 +14,6 @@
 
 //-----------------------------------------------------------------------------
 
-/*
---- RANDONAATTORI ---
-
-This program output to stdout a sequence of characters made by
-cryptographically secure pseudorandom number generator.
-Output could be used as a reasonably good password.
-
-This program use only predefined functions, nothing "home-made stuff".
-
-Compile:
-g++ randonaattori.cpp -o randonaattori
-
-Example usage:
-Calling the program without arguments will output 32 (as default) random characters.
-
-Additional arguments:
--c [number] Set output size according to [number]
--p          Disable defaultSkip. Means that similar looking chars are included.
--d          Output only digits, 0-9
--a          Only a-z and A-Z
--z          Only special chars
-
-randonaattori -pzc 50
-Output 50 random special characters
-*/
-
 #include <random>
 #include <iostream>
 #include <cstring>
